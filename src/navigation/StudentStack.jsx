7d6af3fe0da664e58ac524cc01219c28eb8c7581
Harsh Raj <samarshelley@gmail.com>
2025-07-27 17:07:16 +0530

    removed dup files and updated gitignore

--- src/navigation/StudentStack.jsx
+++ src/navigation/StudentStack.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-// import StudentDashboard from '../screens/StudentDashboard';
-import ComingSoon from '../screens/ComingSoonScreen';
+import StudentDashboard from '../screens/StudentDashboard';
+// import ComingSoon from '../screens/ComingSoonScreen';
 
 const Stack = createNativeStackNavigator();
 
 const StudentStack = () => (
   <Stack.Navigator>
-    <Stack.Screen name="ComingSoon" component={ComingSoon} options={{ headerShown: false }} />
+    <Stack.Screen name="StudentDashboard" component={StudentDashboard} options={{ headerShown: false }} />
   </Stack.Navigator>
 );
 
