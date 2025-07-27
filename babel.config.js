5157be035e639e58456d05c6c25fb2a22cdee230
Harsh Raj <samarshelley@gmail.com>
2025-07-27 23:34:22 +0530

    nativewind setup complete

--- babel.config.js
+++ babel.config.js
@@ -1,19 +1,49 @@
-module.exports = {
-  presets: ['module:@react-native/babel-preset'],
-  plugins: [
-    [
-      'module:react-native-dotenv',
-      {
-        moduleName: '@env',
-        path: '.env',
-        blocklist: null,
-        allowlist: null,
-        blacklist: null, // DEPRECATED
-        whitelist: null, // DEPRECATED
-        safe: false,
-        allowUndefined: true,
-        verbose: false,
-      },
+// module.exports = {
+//   presets: ['module:@react-native/babel-preset'],
+//   plugins: [
+//     [
+//       'module:react-native-dotenv',
+//       {
+//         moduleName: '@env',
+//         path: '.env',
+//         blocklist: null,
+//         allowlist: null,
+//         blacklist: null, // DEPRECATED
+//         whitelist: null, // DEPRECATED
+//         safe: false,
+//         allowUndefined: true,
+//         verbose: false,
+//       },
+//     ],
+//      'nativewind/babel',
+//     'react-native-reanimated/plugin'
+//   ],
+// };
+
+
+
+module.exports = function (api) {
+  api.cache(true);
+  return {
+    presets: ['module:@react-native/babel-preset',
+      'nativewind/babel'    
     ],
-  ],
+    plugins: [
+      [
+        'module:react-native-dotenv',
+        {
+          moduleName: '@env',
+          path: '.env',
+          blocklist: null,
+          allowlist: null,
+          blacklist: null, // DEPRECATED
+          whitelist: null, // DEPRECATED
+          safe: false,
+          allowUndefined: true,
+          verbose: false,
+        },
+      ],
+      'react-native-reanimated/plugin'
+    ],
+  };
 };
--- metro.config.js
+++ metro.config.js
@@ -1,11 +1,16 @@
 const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
-
+const { withNativeWind } = require("nativewind/metro");
 /**
  * Metro configuration
  * https://reactnative.dev/docs/metro
  *
  * @type {import('@react-native/metro-config').MetroConfig}
  */
-const config = {};
+const baseConfig = mergeConfig(getDefaultConfig(__dirname), {
+  // your existing custom resolver or transformer settings go here
+});
 
-module.exports = mergeConfig(getDefaultConfig(__dirname), config);
+// Wrap with NativeWind and point at your Tailwind entry file
+module.exports = withNativeWind(baseConfig, {
+  input: './global.css'
+});
